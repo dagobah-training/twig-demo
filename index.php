bbc122515d64e4837b3a67dabfa8986bdf06760d
Mircha Emanuel D'Angelo <m.dangelo@netsons.com>
2021-05-25 11:56:43 +0200

    esempio for each twig

--- index.php
+++ index.php
@@ -8,15 +8,24 @@ require_once __DIR__ . '/vendor/autoload.php';
 //caricamento engine
 $loader = new FilesystemLoader(__DIR__ . '/templates/');
 $twig = new Environment($loader, [
-    'cache' => __DIR__ . '/templates_c', //cache template compilati
+    //    'cache' => __DIR__ . '/templates_c', //cache template compilati
+    'cache' => false,
 ]);
 
 //caricamento template
 $template = $twig->load('index.html.twig');
 
+//LOGICA
+$users = [
+    ['name' => 'Chiara Cerrone', 'email' => 'chiara@cerrone.it'],
+    ['name' => 'Lorenzo Leccese', 'email' => 'lorenzo@leccese.it'],
+];
+
+
 //variabili template
 $vars = [
-    'page_title' => 'Demo Twig - Stage Netsons'
+    'page_title' => 'Demo Twig - Stage Netsons',
+    'users'      => $users
 ];
 
 echo $template->render($vars);
